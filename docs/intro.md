c0c8300169a89c99feafa507fb960ddd8467e392
Arik Galansky <arik@Ariks-MacBook-Pro.local>
2025-10-05 09:04:29 +0200

    initial revision

---
+++ docs/intro.md
@@ -0,0 +1,6 @@
+---
+sidebar_position: 1
+---
+
+# Introduction
+The Clear Signing Alliance is a cross-ecosystem initiative committed to advancing human-readable and secure transaction signing for blockchain users. Our mission is to replace opaque, hexadecimal signing flows with clear, descriptive representations of transaction intent and parameters. By defining metadata schemas, maintaining an open registry, and providing tooling for wallets and developers, we aim to drive widespread adoption of clear signing. Through this effort, we strive to make signing blockchain operations intuitive, trustworthy, and resistant to user error and prevent some commmon misleading practices and malicious behaviors.
---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,61 @@
+import type {ReactNode} from 'react';
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+  title: string;
+  description: ReactNode;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Metadata registry',
+    description: (
+      <>
+        Create a metadata standard, open registry and open-source tooling for clear signing operations
+      </>
+    ),
+  },
+  {
+    title: 'Metadata creation process',
+    description: (
+      <>
+        Define and operate a process that continualy improves clear signing information for end users
+      </>
+    ),
+  },
+  {
+    title: 'Drive adoption',
+    description: (
+      <>
+        Drive adoption of the standards across all parts of the ecosystem
+      </>
+    ),
+  },
+];
+
+function Feature({title, description}: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): ReactNode {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
